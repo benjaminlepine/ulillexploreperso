419cca2500b7779a316b4feec89615510680ea7a
djenaba bah <djenaba.bah@epitech.eu>
2021-12-22 17:04:45 +0100

    [BUGFIX] generate new android build

--- ulillexplore-wrapper/src/main/java/fr/univlille/ulillexplore/StaticFile.java
+++ ulillexplore-wrapper/src/main/java/fr/univlille/ulillexplore/StaticFile.java
@@ -12,6 +12,13 @@ public class StaticFile {
     @GetMapping("/.well-known/assetlinks.json")
     public ResponseEntity<String> getAssertLingks() {
         String json = "[{\n" +
+                "  \"relation\": [\"delegate_permission/common.handle_all_urls\"],\n" +
+                "  \"target\": {\n" +
+                "    \"namespace\": \"android_app\",\n" +
+                "    \"package_name\": \"com.e.ulillexplore\",\n" +
+                "    \"sha256_cert_fingerprints\":\n" +
+                "    [\"F6:A0:06:0D:70:5C:44:A7:0F:6F:07:17:E0:F9:3D:CB:89:74:8E:73:69:5A:BB:E6:8D:D2:C8:19:C0:8E:4C:53\"]\n" +
+                "  }\n" +
                 "}]";
         final HttpHeaders header = new HttpHeaders();
         header.setContentType(MediaType.APPLICATION_JSON);
